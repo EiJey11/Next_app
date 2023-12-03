4fe0eb35eacb024c408ef7a54b2313cbd0127c1e
EiJey11 <90102698+EiJey11@users.noreply.github.com>
2023-12-03 23:17:27 +0100

    Added getServerSideProps and Posts

---
+++ labb_3/pages/posts/index.js
@@ -0,0 +1,49 @@
+function AllPosts({ posts }) {
+    return (
+        <div className="flex flex-col justify-center items-center w-full min-h-screen">
+            <h1 className="text-3xl font-extrabold mb-8">Posts</h1>
+            <div className="flex flex-wrap justify-center mx-32 my-16">
+                {posts.slice(0, 6).map((post) => {
+                    return (
+                        <div
+                            className="flex flex-col items-start justify-center max-w-[400px] min-h-[240px] m-4 rounded-md shadow-lg py-4 px-8 bg-[#f7fee7]"
+                            key={post.id}
+                        >
+                            <div className="flex">
+                                <h2 className="font-semibold pr-2">Title:</h2>
+                                <h3>{post.title}</h3>
+                            </div>
+                            <div className="flex">
+                                <h2 className="font-semibold pr-2">
+                                    Reaction:
+                                </h2>
+                                <h2 className="font-bold text-[#082f49] ">
+                                    {post.reactions}
+                                </h2>
+                            </div>
+                            <div className="flex flex-col mt-6">
+                                <h2 className="font-semibold pr-2">
+                                    Description:
+                                </h2>
+                                <h2>{post.body.slice(0, 80)} ...</h2>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default AllPosts
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:64868/db.json')
+    const data = await response.json()
+
+    return {
+        props: {
+            posts: data
+        }
+    }
+}
