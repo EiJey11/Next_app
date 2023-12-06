a34520a42bc1cf71fadc83010969dc8a7486b3be
EiJey11 <90102698+EiJey11@users.noreply.github.com>
2023-12-06 12:07:21 +0100

    Added text jest - example

---
+++ labb_3/components/tests/Todo.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Todo from '../../pages/todos/index'
+
+jest.mock('next/router', () => ({
+    useRouter: () => ({
+        pathname: '/',
+        query: ''
+    })
+}))
+
+describe('Todo Component', () => {
+    test('renders Todo component without errors', () => {
+        render(<Todo />)
+    })
+
+    test('updates searchQuery state on input change', () => {
+        const { getByPlaceholderText } = render(<Todo />)
+        const searchInput = getByPlaceholderText('Search...')
+        fireEvent.change(searchInput, { target: { value: 'test' } })
+        expect(searchInput.value).toBe('test')
+    })
+
+    test('updates todos state on search', async () => {
+        const { getByPlaceholderText, getByText } = render(<Todo />)
+        const searchInput = getByPlaceholderText('Search...')
+        fireEvent.change(searchInput, { target: { value: 'test' } })
+
+        const searchButton = getByText('Search')
+        fireEvent.click(searchButton)
+    })
+})
